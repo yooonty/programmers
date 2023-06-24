6bda61a9860714994b5b9b55013e9d99f4aa5b97
Taein Yoon <100198307+yooonty@users.noreply.github.com>
2023-06-24 23:59:41 +0900

    stage1

---
+++ programmers/src/stage1/제일작은수제거하기.java
@@ -0,0 +1,33 @@
+package stage1;
+
+import java.util.*;
+
+public class 제일작은수제거하기 {
+	public int[] solution(int[] arr) {
+		// sort를 이용해서 최솟값 가져오기    
+		int[] temp = arr.clone();
+		Arrays.sort(temp);
+		int min = temp[0];
+		//ArrayList를 이용하여 최솟값과 같지 않은 수만 추가
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] != min) {
+				list.add(arr[i]);
+			}
+		}
+		
+		int[] answer;
+		// 경우를 나눠서 배열에 값을 저장하기		
+		if(list.size() == 0) {
+			answer = new int[1];
+			answer[0] = -1;
+		} else {
+			answer = new int[list.size()];
+			for (int i = 0; i < list.size(); i++) {
+				answer[i] = list.get(i);
+			}
+		}
+		
+        return answer;        
+    }
+}
