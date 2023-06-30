02abc06aa5b6a1c7c0f65275795c2f6762fa556a
Yoon Tae In <yte0130@gmail.com>
2023-06-30 20:08:13 +0900

    stage1

---
+++ programmers/src/stage1/같은숫자는싫어.java
@@ -0,0 +1,20 @@
+package stage1;
+
+import java.util.Stack;
+
+public class 같은숫자는싫어 {
+	public int[] solution(int[] arr) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(arr[0]);
+		for (int i = 1; i < arr.length; i++) {
+			if (stack.peek() != arr[i]) {
+				stack.push(arr[i]);
+			}
+		}
+		int[] answer = new int[stack.size()];
+		for (int i = answer.length - 1; i >= 0; i--) {
+			answer[i] = stack.pop();
+		}
+		return answer;
+	}
+}
