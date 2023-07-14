310b73242740eb449cc056be4cf18779b45cd4b9
Yoon Tae In <yte0130@gmail.com>
2023-07-14 23:48:07 +0900

    stage1

--- programmers/src/stage1/시저암호.java
+++ programmers/src/stage1/시저암호.java
@@ -2,17 +2,20 @@ package stage1;
 
 public class 시저암호 {
 	public String solution(String s, int n) {
-		String answer = "";
 		char[] ch = s.toCharArray();
-		for (char c : ch) {
-			if (c != 32) {
-				if (c == 90 || c == 122)
-					c -= 26;
-				c += n;
-				answer += c;
-			} else
-				answer += " ";
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] == 32) // 공백인 경우
+				continue;
+			else if (65 <= ch[i] && ch[i] <= 90) { // 소문자인 경우
+				ch[i] += n;
+				if (ch[i] > 90) // 범위를 넘어갈때
+					ch[i] -= 26;
+			} else { // 대문자인 경우
+				ch[i] += n;
+				if (ch[i] > 122) // 범위를 넘어갈때
+					ch[i] -= 26;
+			}
 		}
-		return answer;
+		return String.valueOf(ch);
 	}
 }
