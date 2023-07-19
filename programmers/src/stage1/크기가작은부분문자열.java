a8ed1d16e238e335534352f71b333a3c54a463a7
Yoon Tae In <yte0130@gmail.com>
2023-07-19 23:49:41 +0900

    stage1

---
+++ programmers/src/stage1/크기가작은부분문자열.java
@@ -0,0 +1,18 @@
+package stage1;
+
+public class 크기가작은부분문자열 {
+	public int solution(String t, String p) {
+		int length = t.length() - p.length() + 1;
+		long pNum = Long.parseLong(p);
+		int answer = 0;
+
+		for (int i = 0; i < length; i++) {
+			String temp = t.substring(i, i + p.length());
+
+			if (Long.parseLong(temp) <= pNum) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+}
