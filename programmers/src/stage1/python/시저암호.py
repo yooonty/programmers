7a5287a1346b1ba6b7fde67b11efe359adf34eb4
Yoon Tae In <yte0130@gmail.com>
2023-08-15 23:57:45 +0900

    stage1

--- programmers/src/stage1/python/시저암호.py
+++ programmers/src/stage1/python/시저암호.py
@@ -1,9 +1,10 @@
 def solution(s, n):
-    s = list(s)
-    for i in range(len(s)):
-        if s[i].isupper():
-            s[i] = chr((ord(s[i]) - ord("A") + n) % 26 + ord("A"))
-        elif s[i].islower():
-            s[i] = chr((ord(s[i]) - ord("a") + n) % 26 + ord("a"))
-
-    return "".join(s)
+    answer = ""
+    for i in s:
+        if i.isupper():
+            answer += chr((ord(i) - ord("A") + n) % 26 + ord("A"))
+        elif i.islower():
+            answer += chr((ord(i) - ord("a") + n) % 26 + ord("a"))
+        else:
+            answer += " "
+    return answer
